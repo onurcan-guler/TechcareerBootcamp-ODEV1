469cec5bb7be76fd5255a90e92250cedf9baeda8
onurcan-guler <onurcan4423@gmail.com>
2023-03-21 23:12:18 +0300

    initial commit

---
+++ Odev1/odev1.kt
@@ -0,0 +1,52 @@
+package com.example.kotlindersler.Odev1
+
+fun main() {
+    var sehir : String = "Malatya,Elazig,Maras,Adiyaman"
+    var ulke   : String = "Turkiye-America"
+    val telefon : String = "0505-000-1111 "
+    var postaKodu : Int = 87000
+    var email : String = "onurcan4423@gmail.com"
+    var meslek : String = "Software Engineer"
+    var stokMiktrari : Int = 1233
+    var musteriAdi : String = "Onur can"
+    var bakiye : Int = 500
+    var dogumGünü : String = "10-07-2000"
+    var maas : Double = 17.500
+    var medeniDurum : String = "Bekar"
+    var urunYorum : String = "ürün çok güzel."
+    var odemeTarihi : String = "Ödeme tarihi : 10/06/2023"
+    var odeme : Double = 100.999
+    var siparisAdeti : Int = 485
+    var arabaModeli : Int = 2023
+    var kitapAdi : String = "AŞK -- Elif Ş."
+    var yayinlamaTarihi : String = "2009-Mart"
+    var indirimMiktari : Int = 9
+    var odaSayisi: Int = 44
+    var enlem : String = "90°"
+    var boylam :String = "123°"
+    var urunAdi : String = "Octavia"
+    var yemekFiyati : String = "150₺"
+    val marka :String = "Skoda"
+    var muzikAdi : String = "Yıllar Utansın"
+    var vıdeoSuresi : Double = 15.20
+    var urunPuani : Int = 7
+    var resimAdi : String = "Güzel Şehir Malatya "
+    var dosyaFormati : String = "Pdf"
+    var renk : String = "MOR"
+    var renkKodu : String = "  Mor:#660099"
+    var telefonModeli : String = " XİOAMİ redmi 9T"
+    var ekranBoyutu : Double = 6.39
+    var ağırlık : Double = 67.5
+    var ulusalGun : String = "23 Nisan. Ulusal Egemenlik ve Çocuk Bayramı"
+    var tatilGunu : String = "Cumartesi-Pazar"
+    var rezervasyonTarihi : String = "10.02.2023"
+    var sokakAdi : String = "Filistin Sokak"
+    var otobüsHatti : String = "Kayalık-Terminal"
+    var kalanDakika : Int = 58
+    var takipKodu : Int = 11223344
+    var kuponSuresi : String = "10 Gün"
+    var faturaAdresi : String = " Bostbaşı caddesi Filistin Sok no:6 "
+
+}
+
+
